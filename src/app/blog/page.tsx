2fa0f43824327bce490008fcda18fdd6c2437c05
E.J. Perez <ejperez37@gmail.com>
2023-09-14 21:40:31 -0400

    Create secret page, create cards, upload content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,4 @@
-"use client"
-
-import { useTheme } from "next-themes"
-
 export default function Page() {
-    const {theme, setTheme } = useTheme();
     return (
         <p>whaddup</p>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Container from "@/components/ui/container"
 import Image from "next/image"
 import { Separator } from "@/components/ui/separator"
