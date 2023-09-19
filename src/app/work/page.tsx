9952221001a618c77e021fe30878c62ef4c74bda
E.J. Perez <ejperez37@gmail.com>
2023-09-19 17:25:34 -0400

    Add responsive layout and placeholders to work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,3 +1,60 @@
+import Container from '@/components/ui/container'
+import Image from 'next/image'
+import {
+  Card,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from '@/components/ui/card'
+
 export default function Page() {
-    return <p>under construction 🚧 🦺 🏗️ </p>
+  return (
+    <Container>
+      <div className="mx-6 mt-6 text-center lg:text-left">
+        <h2 className="text-3xl font-bold mb-6 underline">my work.</h2>
+        <p>curated web dev work</p>
+      </div>
+      <div className="my-3 mx-3 flex flex-col items-stretch lg:flex-row lg:justify-between">
+        <Card className="flex flex-col items-center mx-3 my-3 text-center lg:text-left">
+          <CardContent>
+            <Image
+              src="/images/vscodestock.jpg"
+              className="py-5"
+              height={400}
+              width={400}
+              alt="placeholder"
+            />
+            <CardTitle>test name 1</CardTitle>
+            <CardDescription>test description of mock project</CardDescription>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col items-center mx-3 my-3 text-center lg:text-left">
+          <CardContent>
+            <Image
+              src="/images/vscodestock.jpg"
+              className="py-5"
+              height={400}
+              width={400}
+              alt="placeholder"
+            />
+            <CardTitle>test name 2</CardTitle>
+            <CardDescription>test description of mock project</CardDescription>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col items-center mx-3 my-3 text-center lg:text-left">
+          <CardContent>
+            <Image
+              src="/images/vscodestock.jpg"
+              className="py-5"
+              height={400}
+              width={400}
+              alt="placeholder"
+            />
+            <CardTitle>test name 3</CardTitle>
+            <CardDescription>test description of mock project</CardDescription>
+          </CardContent>
+        </Card>
+      </div>
+    </Container>
+  )
 }
