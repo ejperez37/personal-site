4c682f041a754d6de5e09809fc4421d73946e113
E.J. Perez <ejperez37@gmail.com>
2023-09-24 19:31:03 -0400

    Add basic skeleton

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Container from '@/components/ui/container'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  return (
+    <Container>
+      <div className="flex flex-col items-center">
+        <Skeleton className="w-[275px] h-[275px] rounded-full my-7" />
+        <Skeleton className="w-[200px] h-[20px] rounded-full" />
+        <Skeleton className="w-[200px] h-[20px] rounded-full" />
+        <Skeleton className="w-[200px] h-[20px] rounded-full" />
+        <Skeleton className="w-[200px] h-[20px] rounded-full" />
+      </div>
+    </Container>
+  )
+}
