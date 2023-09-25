2bd5531244d8f3f14ba8bc35440ca35ec5a02595
E.J. Perez <ejperez37@gmail.com>
2023-09-25 09:47:25 -0400

    Edit loading skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,9 +7,13 @@ export default function Loading() {
       <div className="flex flex-col items-center">
         <Skeleton className="w-[275px] h-[275px] rounded-full my-7" />
         <Skeleton className="w-[200px] h-[20px] rounded-full" />
+        <Skeleton className="w-[300px] h-[20px] rounded-full" />
         <Skeleton className="w-[200px] h-[20px] rounded-full" />
-        <Skeleton className="w-[200px] h-[20px] rounded-full" />
-        <Skeleton className="w-[200px] h-[20px] rounded-full" />
+      </div>
+      <div className="flex justify-around">
+        <Skeleton className="w-[100px] h-[20px] rounded-full" />
+        <Skeleton className="w-[100px] h-[20px] rounded-full" />
+        <Skeleton className="w-[100px] h-[20px] rounded-full" />
       </div>
     </Container>
   )
